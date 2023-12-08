71691fa23369b948666ac7ad0a1951850de1edbd
yunjing zeng <vividjing@gmail.com>
2023-12-08 11:43:25 -0500

    setHeader origin

--- sneatserver2/index.js
+++ sneatserver2/index.js
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
-  next();
-});
-
 app.use(
   cors({
     origin: ["https://sneat-dashboard.vercel.app"],
@@ -28,6 +23,12 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
+  next();
+});
 
 // Routes
 
